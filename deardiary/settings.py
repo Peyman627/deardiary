71fe7a48ee182e11a75809fb185ea9c30924c755
Peyman Hassani <hassani.peyman627@gmail.com>
2021-03-22 16:50:28 +0430

    postgres settings

--- deardiary/settings.py
+++ deardiary/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'deardiary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deardiary',
+        'USER': 'peyman627',
+        'PASSWORD': 'jsmac627',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
